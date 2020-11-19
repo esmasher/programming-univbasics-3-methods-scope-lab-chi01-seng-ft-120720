b80f6ffd6987ac3a6e016ca0ca6233942098aa84
Esma Sherwani <esmasherr@gmail.com>
2020-11-19 04:07:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,6 @@
 
 
-def mario (phrase = "It's-a me, Mario!)
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts mario #{phrase}
-end
-mario
+
 
 def toadstool (status = 'Thank You Mario! But Our Princess Is In Another Castle!')
   puts status
