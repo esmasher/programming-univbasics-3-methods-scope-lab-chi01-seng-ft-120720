dea034895d8235d0ccdfe74ebf94be0f62d6a195
Esma Sherwani <esmasherr@gmail.com>
2020-11-19 04:29:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,5 +17,9 @@ def link (word = "It's Dangerous To Go Alone! Take This.")
 end
 link 
 
+def all_phrases (phrase =  "It's-a me, Mario!", status = 'Thank You Mario! But Our Princess Is In Another Castle!', word = "It's Dangerous To Go Alone! Take This.")
+  puts phrase, status, word
+end
+all_phrases
 
 
