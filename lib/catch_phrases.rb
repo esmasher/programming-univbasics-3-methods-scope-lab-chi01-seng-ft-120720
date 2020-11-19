898af029989657ccbf18fed5edfef1af5396656c
Esma Sherwani <esmasherr@gmail.com>
2020-11-19 04:13:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,5 +9,8 @@ def toadstool (status = 'Thank You Mario! But Our Princess Is In Another Castle!
   puts status
 end
 toadstool
-"
-def link ("It's Dangerous To Go Alone! Take This.")
+
+def link (word = "It's Dangerous To Go Alone! Take This.")
+  puts word
+end
+link 
